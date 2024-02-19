6e9762516708cc94cdbb69a8f9f4be3eaef9ef7b
ziroock <zaprin@collegepulse.com>
2024-02-19 12:35:39 -0800

    add server side action in a component

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 - Next.js caches the pages aggressvelty, to make page to page navigation faster and smoother. It even caches more aggressively in production.
 
 - Next.js requires that error pages are rendered as client components. This is because next.js wants to ensure that the error is handled not only on the server, but also on the client.
+
+- To create a server action function we need to set 'use server' inside of the function. This will tell next.js to run the function on the server. The function also needs to be async.
--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -3,6 +3,24 @@ import ImagePicker from '@/components/Meals/ImagePicker'
 import classes from './page.module.css'
 
 export default function ShareMealPage() {
+  // Next.js will execute this function on the server and it will
+  // automatically receive the formData from the form submission.
+  // We need a name propery for each input in the form to access
+  async function shareMeal(formData) {
+    'use server'
+
+    const meal = {
+      title: formData.get('title'),
+      summary: formData.get('summary'),
+      instructions: formData.get('instructions'),
+      image: formData.get('image'),
+      creator: formData.get('name'),
+      creator_email: formData.get('email'),
+    }
+
+    console.log(meal)
+  }
+
   return (
     <>
       <header className={classes.header}>
@@ -12,7 +30,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -33,14 +51,9 @@ export default function ShareMealPage() {
           </p>
           <p>
             <label htmlFor="instructions">Instructions</label>
-            <textarea
-              id="instructions"
-              name="instructions"
-              rows="10"
-              required
-            ></textarea>
+            <textarea id="instructions" name="instructions" rows="10" required></textarea>
           </p>
-          <ImagePicker />
+          <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
             <button type="submit">Share Meal</button>
           </p>
